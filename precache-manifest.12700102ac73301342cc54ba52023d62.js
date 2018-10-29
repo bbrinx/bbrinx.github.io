7c764b3bfdc29993c6b65e73614110204c2ffaba
Jonathan <joni.derin@gmail.com>
2018-10-29 14:44:14 -0400

    Updates

---
+++ precache-manifest.12700102ac73301342cc54ba52023d62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "820ece13a994d7fe279b",
+    "url": "/static/js/main.820ece13.chunk.js"
+  },
+  {
+    "revision": "ff4e063aa959b6e4f869",
+    "url": "/static/js/1.ff4e063a.chunk.js"
+  },
+  {
+    "revision": "820ece13a994d7fe279b",
+    "url": "/static/css/main.9d0da4d4.chunk.css"
+  },
+  {
+    "revision": "ff4e063aa959b6e4f869",
+    "url": "/static/css/1.d43d1443.chunk.css"
+  },
+  {
+    "revision": "584fe95bf34fbdb7990b776f2b7f59e5",
+    "url": "/index.html"
+  }
+];
