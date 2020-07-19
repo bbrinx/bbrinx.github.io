6a29bbe1ca53554a9d98528de2abd474b4a2c7dc
Jonathan Derin <joni.derin@gmail.com>
2020-07-19 18:29:01 -0400

    Updates

---
+++ precache-manifest.61e8ecb4242c4ded4515498df7c26257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8984497dc2d0cedbd2c8975caaca3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02376eff3742ee1b9118",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "357648163423fc4b6c10",
+    "url": "/static/css/main.23137e06.chunk.css"
+  },
+  {
+    "revision": "02376eff3742ee1b9118",
+    "url": "/static/js/2.e33a2f28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e33a2f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357648163423fc4b6c10",
+    "url": "/static/js/main.b993cd10.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  }
+]);
